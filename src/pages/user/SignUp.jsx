0ce8984b6c6bb0d19fa2ea06fcbab922ddb39edd
mohamdhazem <medozoma0@gmail.com>
2025-10-05 20:27:31 +0300

    handle signup

--- src/pages/user/SignUp.jsx
+++ src/pages/user/SignUp.jsx
@@ -1,12 +1,12 @@
-import { Signupform } from "../../components/user/Login/SignUpForm"
 import { NavBar } from "../../components/user/shared/NavBar"
+import { Signupform } from "../../components/user/Login/Signupform";
 
 export const SignUp = () => {
     return (
         <div className="h-screen overflow-hidden">
             <NavBar></NavBar>
             <hr className="text-gray-200" />
-            <Signupform>SignUpForm</Signupform>
+            <Signupform></Signupform>
         </div>
     )
 }
