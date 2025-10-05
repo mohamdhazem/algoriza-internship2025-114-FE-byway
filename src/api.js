ff70ae87444535e96a405fb09a6fea3a2fa1d777
mohamdhazem <medozoma0@gmail.com>
2025-10-05 20:37:52 +0300

    handle api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
@@ -12,4 +12,3 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-export default api;
